c8f2c6d7ae19c0a136070f9e99385bf9233aeed5
Obed Paz <lesterpaz99@gmail.com>
2021-08-23 16:30:14 -0600

    :sparkles: create the Header template

---
+++ src/template/Header.ts
@@ -0,0 +1,16 @@
+const Header = (): string => {
+	return `
+		<div class="main-header">
+			<div class="header-logo">
+				<h1>
+					<a href="#/">Scientist100</a>
+				</h1>
+			</div>
+			<div class="header-nav">
+				<a href="#/about">About</a>
+			</div>
+		</div>
+	`;
+};
+
+export default Header;
