89fc47381e71f90dd9190e7a37c32146ca2a979b
Obed Paz <lesterpaz99@gmail.com>
2021-08-23 19:28:26 -0600

    :necktie: end up the view logic

---
+++ src/routes/index.routes.ts
@@ -0,0 +1,37 @@
+import Header from '../template/Header';
+import Home from '../views/Home';
+import Characters from '../views/Characters';
+import NotFound from '../views/NotFound';
+import getHash from '../utils/getHash';
+import resolveRoutes from '../utils/resolveRoutes';
+
+interface Pages {
+	[key: string]: () => string | Promise<string>;
+}
+
+const routes: Pages = {
+	'/': Home,
+	'/:id': Characters,
+	NotFound,
+};
+
+const router = async (): Promise<any> => {
+	const header: HTMLElement | null = document.getElementById('header');
+	const content: HTMLElement | null = document.getElementById('content');
+
+	if (header) {
+		header.innerHTML = Header();
+	}
+
+	let hash: string = getHash();
+	let route: string = resolveRoutes(hash);
+	let render: () => string | Promise<string> = routes[route]
+		? routes[route]
+		: routes.NotFound;
+
+	if (content) {
+		content.innerHTML = await render();
+	}
+};
+
+export default router;
