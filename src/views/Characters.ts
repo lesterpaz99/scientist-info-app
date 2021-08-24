2f70530b98224a8031e125d85410fc92edfccb69
Obed Paz <lesterpaz99@gmail.com>
2021-08-23 19:24:06 -0600

    :sparkles: fetch data and show it in both views

--- src/views/Characters.ts
+++ src/views/Characters.ts
@@ -1,17 +1,26 @@
-const Characters = (): string => {
+import getHash from '../utils/getHash';
+import getData from '../utils/getData';
+
+const Characters = async (): Promise<string> => {
+	const id = getHash();
+	const character = await getData(id);
+
+	const { image, name, status, species, gender, origin, location }: any =
+		character;
+
 	return `
 		<div class="character-inner">
 			<article class="character-card">
-				<img src="image" alt="name" />
-				<h2>Name</h2>
+				<img src="${image}" alt="${name}" />
+				<h2>${name}</h2>
 			</article>
 			<article class="character-card">
-				<h3>Episodes:</h3>
-				<h3>Status:</h3>
-				<h3>Species:</h3>
-				<h3>Gender:</h3>
-				<h3>Origin:</h3>
-				<h3>Last location:</h3>
+				<h3>Episodes: </h3>
+				<h3>Status: ${status}</h3>
+				<h3>Species: ${species}</h3>
+				<h3>Gender: ${gender}</h3>
+				<h3>Origin: ${origin.name}</h3>
+				<h3>Last location: ${location.name}</h3>
 			</article>
 		</div>
 	`;
--- src/views/Home.ts
+++ src/views/Home.ts
@@ -1,14 +1,25 @@
-const Home = (): string => {
-	return `
-		<div class="characters">
-			<article class="character-item">
-				<a href="#/1/">
-					<img src="image" alt="name" />
-					<h2>Name</h2>
-				</a>
-			</article>
-		</div>
-	`;
+import getData from '../utils/getData';
+
+const content: HTMLElement | null = document.getElementById('content');
+
+const Home = async (): Promise<string> => {
+	const characters = await getData();
+	const { results }: any = characters;
+
+	const card = document.createElement('div');
+	card.classList.add('characters');
+
+	const view: Array<any> = (card.innerHTML = results.map(
+		(character: any) => `
+		<article class="character-item">
+			<a href="#/${character.id}/">
+				<img src="${character.image}" alt="name" />
+				<h2>${character.name}</h2>
+			</a>
+		</article>
+		`
+	));
+	return view.join();
 };
 
 export default Home;
