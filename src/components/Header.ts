d86077804db75a69d0f5b5db556d10c1781a2df1
Obed Paz <lesterpaz99@gmail.com>
2021-11-14 21:48:01 -0600

    end up the Header component

--- src/components/Header.ts
+++ src/components/Header.ts
@@ -1,15 +1,18 @@
+import Logo from '../assets/logoRandM.png';
+
 const Header = (): string => {
 	return `
-		<header class="main-header">
-			<div class="header-logo">
-				<h1>
-					<a href="#/">Scientist100</a>
-				</h1>
+			<div class="title-container">
+				<a>
+					<a href="/">Home</a>
+				</a>
+			</div>
+			<div class="logo-container">
+				<img src="${Logo}" alt="logo">
 			</div>
 			<div class="header-nav">
 				<a href="#/about">About</a>
 			</div>
-		</header>
 	`;
 };
 
